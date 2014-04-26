8117fa5d09f2cff4d5c3cc9ac4cb3ad50620adcd
Jong Eun Lee <yomybaby@yomybaby.com>
2014-04-26 16:01:50 +0900

    dev.xxx property for development deploy type

--- plugin/yy.ticonfig/hooks/ticonfig.js
+++ plugin/yy.ticonfig/hooks/ticonfig.js
@@ -6,10 +6,19 @@ exports.init = function (logger, config, cli, appc) {
   // credits to @fokkezb for pointing out the build.pre.contruct hook - https://github.com/dbankier/TiShadow/pull/213/
   cli.addHook('build.pre.construct', function (build, finished) {
     if (build.tiapp && build.tiapp.properties) {
+      // cli.xxx
       var keys = _.keys(build.tiapp.properties).filter(function(e) { return e.match("^cli\.");});
       keys.forEach(function(k) {
         build[k.replace(/^cli\./,'')] = build.tiapp.properties[k].value;
       });
+
+      // dev.xxx
+      if(build.deployType !== "production"){
+        var devKeys = Object.keys(build.tiapp.properties).filter(function(e) { return e.match("^dev\.");});
+        devKeys.forEach(function(k) {
+          build.tiapp.properties[k.replace(/^dev\./, '')].value = build.tiapp.properties[k].value;
+        });
+      }
     }
     finished();
   });
